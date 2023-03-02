bde2c2d16890f4ae3518f1d1a2849cce4ec10254
le0-vv <pyleow@icloud.com>
2023-03-02 15:42:00 +1100

    w2 r pt2 st1 done

--- W2/Required/2 Conditionals/Step 1/sketch.js
+++ W2/Required/2 Conditionals/Step 1/sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  // This is just setup code, making the canvas the right size and the 
+  // background a pleasantly numbing shade of grey.
+  createCanvas(windowWidth, windowHeight);
+  background(225);
+  print(windowWidth, windowHeight)
 }
 
 function draw() {
-  background(220);
+  // This draws a red line in the middle of the canvas
+  // (it should also give you a clue for your condition!)
+  // You might hear this referred to as a "debug view";
+  // you can remove it from the final design, but it will help you see
+  // how the code is working, which helps remove bugs!
+  stroke(255,0,0);
+  line(width/2, 0, width/2, height);
+  stroke(0);
+
+  noFill();
+
+  // Put your condition here, in parentheses after the if (this one isn't quite right!)
+  if(mouseX > width/2) {
+    rectMode(CENTER)
+    square(mouseX,mouseY,100,)
+    // Put what happens if your condition is true here
+  } else {
+    circle(mouseX,mouseY,100)
+		// Put what happens if your condition is false here
+  }
 }
