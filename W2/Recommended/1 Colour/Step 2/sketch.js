b94d57a738840d0a78660733b26b5bc1b04da765
le0-vv <pyleow@icloud.com>
2023-03-02 16:23:24 +1100

    optimising previous commit

--- W2/Recommended/1 Colour/Step 2/sketch.js
+++ W2/Recommended/1 Colour/Step 2/sketch.js
@@ -3,15 +3,16 @@ function setup() {
 }
 
 function draw() {
+  let increment = width/5
   let baseX = 0
-  let toX = width/5
+  let toX = increment
   for (let x = 0; x <= 160; x+=40){
     for (let i = 0; i <= height; i++) {
       let redAmount = map(i, 0, height, x, 255);
       stroke(redAmount, 100, 200);
       line(baseX,i,toX,i)
     }
-    baseX += width/5
-    toX += width/5
+    baseX += increment
+    toX += increment
   }
 }
