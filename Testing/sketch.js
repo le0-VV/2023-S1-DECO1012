e1c949f92fcbc57d0201e42554bd8ed5e5939b10
le0-vv <pyleow@icloud.com>
2023-03-02 17:36:40 +1100

    fiddled around with testing ground, might gitignore it later

--- Testing/sketch.js
+++ Testing/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(100)
+  fill(200)
+  square(0,0,100)
 }
 
 function draw() {
-  rect(0, 0, width/2, height/2);
+  // print(map(35, 25, 50, 50, 25))
+  let x = Image
 }
