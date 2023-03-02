a396dc091e37826f547d4965a5294d2e41eef4dd
le0-vv <pyleow@icloud.com>
2023-03-02 16:29:59 +1100

    funsies

--- W2/Recommended/1 Colour/Step 2/sketch.js
+++ W2/Recommended/1 Colour/Step 2/sketch.js
@@ -1,18 +1,21 @@
+let colourMax = 160
+let overallIncrement = 5
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  let increment = width/5
+  let screenIncrement = width/overallIncrement
   let baseX = 0
-  let toX = increment
-  for (let x = 0; x <= 160; x+=40){
+  let toX = screenIncrement
+  for (let x = 0; x <= colourMax; x+=colourMax/overallIncrement){
     for (let i = 0; i <= height; i++) {
       let redAmount = map(i, 0, height, x, 255);
       stroke(redAmount, 100, 200);
       line(baseX,i,toX,i)
     }
-    baseX += increment
-    toX += increment
+    baseX += screenIncrement
+    toX += screenIncrement
   }
 }
